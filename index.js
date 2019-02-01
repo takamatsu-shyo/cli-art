209c042f682366d5e617c7db5a2acab3f5c0b14e
takamatsu-shyo <takamatsu.shyo@gmail.com>
2019-02-02 00:13:02 +0200

    Feat: random

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const https = require('https');
 
+let number = Math.floor(Math.random() * 64)
+let url = 'https://www.asciipr0n.com/pr0n/anime/anime' + number.toString().padStart(2,'0') + '.txt'
 
-
-https.get('https://www.asciipr0n.com/pr0n/anime/anime40.txt', (resp) => {
+//https.get('https://www.asciipr0n.com/pr0n/anime/anime40.txt', (resp) => {
+https.get(url, (resp) => {
 		let data = '';
 
 		resp.on('data', (chunk) => {
@@ -13,6 +15,8 @@ https.get('https://www.asciipr0n.com/pr0n/anime/anime40.txt', (resp) => {
 
 		resp.on('end', () => {
 			console.log(data);		
+			console.log(url);		
 		})
+
 });
 

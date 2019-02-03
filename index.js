e782d35d691335767d6f58a659af45d9cda0b086
takamatsu-shyo <takamatsu.shyo@gmail.com>
2019-02-03 14:06:42 +0200

    Feat: All genre

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 #!/usr/bin/env node
 
+/*
+TODO README
+TODO All genre
+TODO Option -y
+TODO Option -p, -a, -m, -h
+*/
+
 const https = require('https');
 
-let number = Math.floor(Math.random() * 64)
-let url = 'https://www.asciipr0n.com/pr0n/anime/anime' + number.toString().padStart(2,'0') + '.txt'
+function gen_url(genre){
 
-//https.get('https://www.asciipr0n.com/pr0n/anime/anime40.txt', (resp) => {
-https.get(url, (resp) => {
+	let umber, url;
+
+	switch(genre){
+		case 'p':
+			// Pinup: 00-47
+			number = Math.floor(Math.random() * 47)
+			url = 'https://www.asciipr0n.com/pr0n/pinups/pinup' + number.toString().padStart(2,'0') + '.txt'
+			break;
+		
+		case 'a':
+			// Animation: 00-62
+			number = Math.floor(Math.random() * 64)
+			url = 'https://www.asciipr0n.com/pr0n/anime/anime' + number.toString().padStart(2,'0') + '.txt'
+			break;
+		
+		case 'h':
+			// Hunk: 00-08
+			number = Math.floor(Math.random() * 8)
+			url = 'https://www.asciipr0n.com/pr0n/hunky/hunky' + number.toString().padStart(2,'0') + '.txt'
+			break;
+		
+		case 'm':
+			// More: 00-94
+			number = Math.floor(Math.random() * 94)
+			url = 'https://asciipr0n.com/pr0n/morepr0n/pr0n' + number.toString().padStart(2,'0') + '.txt'
+			break;
+		
+		default:
+			url = 'https://asciipr0n.com/pr0n/pinups/venus01.txt'
+	}
+
+	return url
+};
+
+var gen_sw = ['p', 'a', 'h', 'm'];
+
+https.get(url = gen_url( gen_sw[ Math.floor(Math.random() * 3 ) ] ), (resp) => {
+
 		let data = '';
 
 		resp.on('data', (chunk) => {
